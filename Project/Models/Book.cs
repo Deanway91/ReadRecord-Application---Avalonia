467ded22849799daac1e52f0f948aab4cfa4ab81
Deanway91 <101314750+Deanway91@users.noreply.github.com>
2025-04-30 11:57:38 +0800

    Starting Project -todo scrollview on main page

---
+++ Project/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using  CommunityToolkit.Mvvm.ComponentModel;
+    
+namespace Project.Models;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public List<string> Tags;
+}
---
+++ Project/Models/TemplatedBookViewControl.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Project.Models;
+
+public class TemplatedBookViewControl : TemplatedControl
+{
+}
